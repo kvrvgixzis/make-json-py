20788c6830427637e470ebd8297811fd6e86214f
Nikita Parfenov <n1koss.boy@gmail.com>
2019-08-05 22:42:13 +0500

    v1.1

--- make_json.py
+++ make_json.py
@@ -8,14 +8,11 @@ Ts, X, Y, s = 0.0, 0.0, 0.0, 0.0
 
 for root, dirs, filenames in os.walk(indir):
     for f in filenames:
-        temp = 0
         export_data = []
         importfile = open(os.path.join(root, f), 'r')
-        for lines in importfile:
-            temp += 1
-            if temp > 2:
-                Ts, X, Y, s = lines.split()
-                export_data.append({"X": float(X), "Y": float(Y), "Ts": float(Ts) / 1000.0}) 
+        for lines in importfile[3:]:
+            Ts, X, Y, s = lines.split()
+            export_data.append({"X": float(X), "Y": float(Y), "Ts": float(Ts) / 1000.0}) 
         importfile.close()  
 
         print("sdfsd" , f)
